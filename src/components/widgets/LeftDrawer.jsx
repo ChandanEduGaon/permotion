48da8ea52c4ed0b56a7e4e67bab89c71a842d954
Chandan Kumar <singh963130@gmail.com>
2023-10-09 01:00:31 +0530

    UI Upgraded

--- src/components/widgets/LeftDrawer.jsx
+++ src/components/widgets/LeftDrawer.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-import { Drawer, Sidenav, Nav } from "rsuite";
-import DashboardIcon from "@rsuite/icons/legacy/Dashboard";
-import GroupIcon from "@rsuite/icons/legacy/Group";
-import MagicIcon from "@rsuite/icons/legacy/Magic";
-import GearCircleIcon from "@rsuite/icons/legacy/GearCircle";
+import { Drawer } from "rsuite";
 
 const LeftDrawer = () => {
   const [open, setOpen] = React.useState(false);
